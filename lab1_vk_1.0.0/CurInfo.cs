8bc4d7347f845123dca6f071e81a97acdf741100
user-ПК\user <a_ivch13066@mail.ru>
2017-09-21 04:22:32 +0300

    Консольная версия, ничего не работает, но запрос-ответ есть.

---
+++ lab1_vk_1.0.0/CurInfo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab1_vk_1._0._0
+{
+    public class CurInfo
+    {
+        public string Cur_Abbreviation { get; set; }
+        public string Cur_OfficialRate { get; set; }
+    }
+}
---
+++ lab1_vk_1.0.0/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.IO;
+
+namespace lab1_vk_1._0._0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string urlBegin = "http://www.nbrb.by/API/ExRates/Rates/";
+            string[] urlCode = new string[] { "840", "978", "643" };
+            string urlEnd = "?ParamMode=1";
+
+            for (int i = 0; i < urlCode.Length; i++)
+            {
+                string url = urlBegin + urlCode[i] + urlEnd;
+                HttpWebRequest httpWebRequest = (HttpWebRequest)WebRequest.Create(url);
+                HttpWebResponse httpWebResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+                string response;
+                using (StreamReader streamReader = new StreamReader(httpWebResponse.GetResponseStream()))
+                {
+                    response = streamReader.ReadToEnd();
+                }
+            }
+            
+        }
+    }
+}
